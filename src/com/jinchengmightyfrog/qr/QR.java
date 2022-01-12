7d473837cfdaab0f15f079a83fe1491241f7b2f4
Jin Cheng <cj21@foxmail.com>
2022-01-12 00:52:52 +0000

    Initial commit

---
+++ src/com/jinchengmightyfrog/qr/QR.java
@@ -0,0 +1,9 @@
+package com.jinchengmightyfrog.qr;
+
+import qr.CreateQR;
+
+public class QR {
+    public static void main(String[] args) {
+        CreateQR.make(120,120,"https://github.com/Mighty-Frog", "C:\\Users\\ASUS\\Pictures\\qr.png");
+    }
+}
